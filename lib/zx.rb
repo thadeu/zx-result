d88f9ad3828d051a793a4f8d8cfebb2f07f1c8c1
thadeu <tadeuu@gmail.com>
2024-05-07 03:39:37 +0000

    fix: included and extend Methods

--- lib/zx.rb
+++ lib/zx.rb
@@ -6,24 +6,29 @@ require 'zx/reflect'
 require 'zx/result'
 
 module Zx
-  Success = ->(value = nil, options = {}) { Zx.Success(value, { type: :ok }.merge(options)) }
-  Failure = ->(value = nil, options = {}) { Zx.Failure(value, { type: :error }.merge(options)) }
-
-  def Success(value = nil, options = {})
-    Zx::Result.new.success!(value, type: options.fetch(:type, :ok))
+  module Methods
+    Success = ->(value = nil, options = {}) { Zx.Success(value, { type: :ok }.merge(options)) }
+    Failure = ->(value = nil, options = {}) { Zx.Failure(value, { type: :error }.merge(options)) }
+  
+    def Success(value = nil, options = {})
+      Zx::Result.new.success!(value, type: options.fetch(:type, :ok))
+    end
+  
+    def Failure(value = nil, options = {})
+      Zx::Result.new.failure!(value, type: options.fetch(:type, :error))
+    end
+  
+    def Try(default = nil, options = {})
+      Success[yield]
+    rescue StandardError => e
+      Failure[default || options.fetch(:or, nil)]
+    end
+  
+    def Given(input)
+      Try { input }
+    end
   end
-
-  def Failure(value = nil, options = {})
-    Zx::Result.new.failure!(value, type: options.fetch(:type, :error))
-  end
-
-  def Try(default = nil, options = {})
-    Success[yield]
-  rescue StandardError => e
-    Failure[default || options.fetch(:or, nil)]
-  end
-
-  def Given(input)
-    Try { input }
-  end
+  
+  include Methods
+  extend Methods
 end
