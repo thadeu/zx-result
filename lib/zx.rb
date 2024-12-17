8dc645284ec7c8e2699a89423f6b78567d6155ba
thadeu <tadeuu@gmail.com>
2024-12-16 22:51:41 -0300

    added Core class

--- lib/zx.rb
+++ lib/zx.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'zx/version'
+require 'zx/core'
 require 'zx/core/util/parameter'
 require 'zx/core/base'
 require 'zx/core/and_then'
---
+++ lib/zx/core.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Zx
+  module Core
+  end
+end
