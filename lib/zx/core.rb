516f651c6615a550504a6d06dc40497eecc22aba
thadeu <tadeuu@gmail.com>
2024-12-16 22:45:17 -0300

    fix require core class

--- lib/zx/core.rb
+++ lib/zx/core.rb
@@ -2,11 +2,12 @@
 
 module Zx
   module Core
-    require_relative 'core/util/parameter'
-    require_relative 'core/base'
-    require_relative 'core/and_then'
-    require_relative 'core/caller'
-    require_relative 'core/match'
-    require_relative 'core/stack'
   end
 end
+
+require_relative 'core/util/parameter'
+require_relative 'core/base'
+require_relative 'core/and_then'
+require_relative 'core/caller'
+require_relative 'core/match'
+require_relative 'core/stack'
